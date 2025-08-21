b335ebc1ca6cccd10712fcdab822c77b60b193d5
Husky <montalvo05.ac@gmail.com>
2025-08-20 18:58:00 -0600

    Crear superusuario automáticamente en ventas

--- ventas/apps.py
+++ ventas/apps.py
@@ -4,3 +4,18 @@ from django.apps import AppConfig
 class VentasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ventas'
+    
+from django.contrib.auth.models import User
+
+class MiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mi_app'  # Asegúrate de que coincida con el nombre de tu app
+
+    def ready(self):
+        # Crear superusuario automáticamente si no existe
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                "admin", 
+                "admin@example.com", 
+                "nitram"
+            )
