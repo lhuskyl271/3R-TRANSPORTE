426273193c6b3f3b4d0c6e57b3d73cd4aa100179
Husky <montalvo05.ac@gmail.com>
2025-08-26 03:33:57 -0600

    Corregir estructura de archivos estáticos

--- mi_crm/urls.py
+++ mi_crm/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from ventas import views as ventas_views
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
